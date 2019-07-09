149a500e0d92b1e7349e75d9cbeae1254b867497
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-09 20:12:38 +0700

    updsate

---
+++ src/components/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-toolbar flat dark color="teal">
+      <v-toolbar-side-icon />
+      <v-toolbar-title>This is MKN</v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class AppBar extends Vue {}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-container class="home">
+    <HelloWorld />
+  </v-container>
 </template>
 
 <script lang="ts">
