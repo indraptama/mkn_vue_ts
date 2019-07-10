6debd08987893d0816d090b702f68da2bc266edf
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-10 15:35:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <v-app>
     <AppBar />
-
-    <v-content>
-      <router-view />
+    <v-content class="pt-5">
+      <v-layout fill-height>
+        <v-flex shrink>
+          <v-navigation-drawer :mini-variant.sync="mini"></v-navigation-drawer>
+        </v-flex>
+        <v-flex>
+          <router-view />
+        </v-flex>
+      </v-layout>
     </v-content>
   </v-app>
 </template>
@@ -11,15 +17,22 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import AppBar from "@/components/AppBar.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 @Component({
   components: {
-    AppBar,
-    HelloWorld
+    AppBar
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  // state
+  mini = true;
+  drawer = true;
+  right = null;
+  items = [
+    { title: "Home", icon: "dashboard" },
+    { title: "About", icon: "question_answer" }
+  ];
+}
 </script>
 
 <style></style>
--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-toolbar flat dark color="teal">
-      <v-toolbar-side-icon />
+    <v-toolbar flat dark fixed color="teal" style="z-index: 5">
+      <v-toolbar-side-icon @click.stop="leftMenuClick()" />
       <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
       <v-spacer />
       <v-btn icon>
@@ -11,21 +11,39 @@
         <v-icon>apps</v-icon>
       </v-btn>
 
-      <v-menu :nudge-width="100">
+      <v-menu nudge-width="100" nudge-bottom="16">
         <template v-slot:activator="{ on }">
           <div class="ml-2" v-on="on">
             <v-avatar size="32">
               <img src="avatar.jpg" />
             </v-avatar>
-            <v-icon dark>arrow_drop_down</v-icon>
           </div>
         </template>
 
-        <v-list>
-          <v-list-tile v-for="item in items" :key="item" @click.prevent>
-            <v-list-tile-title v-text="item"></v-list-tile-title>
-          </v-list-tile>
-        </v-list>
+        <v-card>
+          <v-list>
+            <v-list-tile avatar class="mb-2">
+              <v-list-tile-avatar>
+                <img src="avatar.jpg" alt="John" />
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title>Indra Pratama Putra</v-list-tile-title>
+                <v-list-tile-sub-title
+                  >Notaris/PPAT Kabupaten Garut</v-list-tile-sub-title
+                >
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-divider />
+            <v-list-tile v-for="item in profileMenu" :key="item" @click.prevent>
+              <v-list-tile-title v-text="item"></v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+          <v-divider />
+          <v-card-actions>
+            <v-btn depressed block large color="primary">Log Out</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-menu>
     </v-toolbar>
   </div>
@@ -36,7 +54,13 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class AppBar extends Vue {
-  items = ["All", "Family", "Friends", "Coworkers"];
+  // data
+  menuLeftStatus = "closed";
+  profileMenu = ["All", "Family", "Friends", "Coworkers"];
+
+  leftMenuClick() {
+    console.log(this.menuLeftStatus);
+  }
 }
 </script>
 
