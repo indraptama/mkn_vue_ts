81d37700792d9b463d7bef75c925a2556e7f3c85
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-12 16:50:14 +0700

    update

---
+++ src/components/CardQuickStart.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card height="160" :color="linkList.color" dark hover :to="linkList.link">
+    <v-img height="80" :src="linkList.img"></v-img>
+    <v-card-text>
+      <span class="subheading font-weight-medium">{{ linkList.title }}</span>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    linkList: {
+      default: {}
+    }
+  }
+})
+export default class CardQuickStart extends Vue {}
+</script>
+
+<style scoped></style>
