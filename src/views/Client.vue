9a5ebaf23a7961ad5ad3820509fe362391ee0822
Indra Pratama Putra <indra.symfonia@gmail.com>
2019-07-16 06:06:52 +0700

    Update Client.vue

--- src/views/Client.vue
+++ src/views/Client.vue
@@ -5,48 +5,92 @@
     </v-flex>
 
     <v-flex grow>
-      <v-container>
-        <v-card color="teal" dark flat>
-          <v-img height="200">
-            <v-layout align-end fill-height>
-              <v-flex pa-4>
-                <v-layout align-end>
-                  <v-avatar size="80">
-                    <v-img :src="personData[0].avatar" />
-                  </v-avatar>
-                  <div class="pl-3 align-self-end">
-                    <h1 class="title mb-1">{{ personData[0].fullName }}</h1>
-                    <h2 class="body-1 mb-1">{{ personData[0].nik }}</h2>
-                    <div>
-                      <v-icon small>place</v-icon>
-                      {{
+      <v-card color="blue darken-5" dark flat>
+        <v-img height="200">
+          <v-layout align-end fill-height>
+            <v-flex pa-4>
+              <v-layout align-end>
+                <v-avatar size="80">
+                  <v-img :src="personData[0].avatar" />
+                </v-avatar>
+                <div class="pl-3 align-self-end">
+                  <h1 class="title mb-1">{{ personData[0].fullName }}</h1>
+                  <h2 class="body-1 mb-1">{{ personData[0].nik }}</h2>
+                  <div>
+                    <v-icon small>place</v-icon>
+                    {{
                       personData[0].address.kecamatan +
-                      ", " +
-                      personData[0].address.city
-                      }}
-                    </div>
+                        ", " +
+                        personData[0].address.city
+                    }}
                   </div>
-                  <v-spacer />
-                  <div class="align-self-end">
-                    <v-layout>
-                      <v-flex pl-2 pr-2>
-                        <h4>12</h4>
-                        <div class="caption">akta</div>
-                      </v-flex>
-                      <v-flex pl-2 pr-2>
-                        <h4>19</h4>
-                        <div class="caption">Menerima Berkas</div>
-                      </v-flex>
-                      <v-flex pl-2 pr-2>
-                        <h4>12 Juni 2019</h4>
-                        <div class="caption">Terakhir Menghadap</div>
-                      </v-flex>
-                    </v-layout>
+                </div>
+                <v-spacer />
+                <div class="align-self-end">
+                  <v-layout>
+                    <v-flex pl-2 pr-2>
+                      <h4>12</h4>
+                      <div class="caption">akta</div>
+                    </v-flex>
+                    <v-flex pl-2 pr-2>
+                      <h4>19</h4>
+                      <div class="caption">Menerima Berkas</div>
+                    </v-flex>
+                    <v-flex pl-2 pr-2>
+                      <h4>12 Juni 2019</h4>
+                      <div class="caption">Terakhir Menghadap</div>
+                    </v-flex>
+                  </v-layout>
+                </div>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-img>
+      </v-card>
+      <!-- Profile Card -->
+
+      <v-container justify-center>
+        <v-card max-width="1024">
+          <v-layout fill-height>
+            <!-- Left -->
+            <v-flex>
+              <v-layout align-center pa-5>
+                <v-card width="96" height="96">
+                  <v-img :src="personData[0].avatar" />
+                </v-card>
+                <div class="pl-3">
+                  <h1 class="title mb-1">{{ personData[0].fullName }}</h1>
+                  <h2 class="body-1 mb-2">{{ personData[0].nik }}</h2>
+                  <div>
+                    <v-icon small>place</v-icon>
+                    {{
+                      personData[0].address.kecamatan +
+                        ", " +
+                        personData[0].address.city
+                    }}
                   </div>
-                </v-layout>
-              </v-flex>
-            </v-layout>
-          </v-img>
+                </div>
+              </v-layout>
+              <v-divider />
+              <v-toolbar flat dense class="pl-4 pr-4">
+                <h5>Profile</h5>
+              </v-toolbar>
+              <v-list>
+                <v-list-tile class="pl-5 pr-5">{{
+                  personData[0].fullName
+                }}</v-list-tile>
+              </v-list>
+            </v-flex>
+            <!-- Right -->
+            <v-divider vertical />
+            <v-flex class="bl grey lighten-4">
+              <div class="pa-5 white">
+                <h4>History</h4>
+              </div>
+              <v-divider />
+              <div class="pa-5">lalala</div>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-container>
     </v-flex>
@@ -89,4 +133,8 @@ export default class Client extends Vue {
 }
 </script>
 
-<style></style>
+<style scouped>
+.bl {
+  border-left: 1px solid rgba(0, 0, 0, 0.12);
+}
+</style>
